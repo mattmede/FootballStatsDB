67b99a79851d12157781b8d1d59b1de937caf38a
Nathan Stackpole <nstackpole21@gmail.com>
2017-11-18 11:39:42 -0500

    Added a test project with a test for inserting a player

---
+++ FootballStats/ManipulatorTest/InsertTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DatabaseManipulator;
+using DatabaseModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ManipulatorTest
+{
+    [TestClass]
+    public class InsertTest
+    {
+        [TestMethod]
+        public void InesrtTest1()
+        {
+            Player player = new Player(44, "Football Guy");
+            PlayerManipulator manipulator = new PlayerManipulator();
+            manipulator.Insert(player);
+        }
+    }
+}
---
+++ FootballStats/ManipulatorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ManipulatorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ManipulatorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("130e3320-42b5-4785-b690-e7cd1ec55563")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
