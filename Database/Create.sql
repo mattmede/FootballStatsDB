9f9bb11c5c4bb3b0061056333fc4d5aaff472b1e
Matt <mam361@zips.uakron.edu>
2017-11-19 15:57:39 -0500

    Added auto increment to create tables

--- Database/Create.sql
+++ Database/Create.sql
@@ -1,34 +1,34 @@
 CREATE TABLE Users(
-    User_Id INT PRIMARY KEY,
+    User_Id INT IDENTITY(1,1) PRIMARY KEY,
     Username VARCHAR(50) NOT NULL UNIQUE,
     Password VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Players(
-    Player_Id INT PRIMARY KEY,
+    Player_Id INT IDENTITY(1,1) PRIMARY KEY,
     Name VARCHAR(50) NOT NULL,
     Number INT NOT NULL
 );
 
 CREATE TABLE Teams(
-    Team_Id INT PRIMARY KEY,
+    Team_Id INT IDENTITY(1,1) PRIMARY KEY,
     Name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Fav_Player(
-    FP_Id INT PRIMARY KEY,
+    FP_Id INT IDENTITY(1,1) PRIMARY KEY,
     Player_Id INT FOREIGN KEY REFERENCES Players(Player_Id) NOT NULL,
     User_Id INT FOREIGN KEY REFERENCES Users(User_Id) NOT NULL
 );
 
 CREATE TABLE Fav_Team(
-    FT_Id INT PRIMARY KEY,
+    FT_Id INT IDENTITY(1,1) PRIMARY KEY,
     Team_Id INT FOREIGN KEY REFERENCES Teams(Team_Id) NOT NULL,
     User_Id INT FOREIGN KEY REFERENCES Users(User_Id) NOT NULL
 );
 
 CREATE TABLE Player_Stats(
-    Player_Stat_Id INT PRIMARY KEY,
+    Player_Stat_Id INT IDENTITY(1,1) PRIMARY KEY,
     Week INT NOT NULL,
     Year INT NOT NULL,
     Rush_Yards INT,
@@ -44,7 +44,7 @@ CREATE TABLE Player_Stats(
 );
 
 CREATE TABLE Team_Stats(
-    Team_Stat_Id INT PRIMARY KEY,
+    Team_Stat_Id INT IDENTITY(1,1) PRIMARY KEY,
     Week INT NOT NULL,
     Year INT NOT NULL,
     Wins INT,
@@ -59,13 +59,13 @@ CREATE TABLE Team_Stats(
 );
 
 CREATE TABLE Player_Plays(
-    PP_Id INT PRIMARY KEY,
+    PP_Id INT IDENTITY(1,1) PRIMARY KEY,
     Player_Id INT FOREIGN KEY REFERENCES Players(Player_Id) NOT NULL,
     Player_Stat_Id INT FOREIGN KEY REFERENCES Player_Stats(Player_Stat_Id) NOT NULL
 );
 
 CREATE TABLE Team_Plays(
-    TP_Id INT PRIMARY KEY,
+    TP_Id INT IDENTITY(1,1) PRIMARY KEY,
     Team_Id INT FOREIGN KEY REFERENCES Teams(Team_Id) NOT NULL,
     Team_Stat_Id INT FOREIGN KEY REFERENCES Team_Stats(Team_stat_Id) NOT NULL
 );
