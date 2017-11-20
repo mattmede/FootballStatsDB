a320ff828acaa9ee0b02aa506d06de85df3683fd
Nathan Stackpole <nstackpole21@gmail.com>
2017-11-19 20:17:55 -0500

    Made an insert test for all models

--- FootballStats/ManipulatorTest/InsertTest.cs
+++ FootballStats/ManipulatorTest/InsertTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using DatabaseManipulator;
+using DatabaseManipulation;
 using DatabaseModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -9,11 +10,77 @@ namespace ManipulatorTest
     public class InsertTest
     {
         [TestMethod]
-        public void InesrtTest1()
+        public void Insert_Player_Test()
         {
             Player player = new Player(44, "Football Guy");
-            PlayerManipulator manipulator = new PlayerManipulator();
+            Database_Manipulator manipulator = new Database_Manipulator();
             manipulator.Insert(player);
         }
+
+        [TestMethod]
+        public void Insert_Team_Test()
+        {
+            Team team = new Team("Football Team");
+            Database_Manipulator manipulator = new Database_Manipulator();
+            manipulator.Insert(team);
+        }
+
+        [TestMethod]
+        public void Insert_User_Test()
+        {
+            User user = new User("User", "Password");
+            Database_Manipulator manipulator = new Database_Manipulator();
+            manipulator.Insert(user);
+        }
+
+        [TestMethod]
+        public void Insert_Fav_Player_Test()
+        {
+            Fav_Player fav_player = new Fav_Player(1,1);
+            Database_Manipulator manipulator = new Database_Manipulator();
+            manipulator.Insert(fav_player);
+        }
+
+        [TestMethod]
+        public void Insert_Fav_Team_Test()
+        {
+            Fav_Team fav_team = new Fav_Team(1, 1);
+            Database_Manipulator manipulator = new Database_Manipulator();
+            manipulator.Insert(fav_team);
+        }
+
+        [TestMethod]
+        public void Insert_Player_Stat_Test()
+        {
+            Player_Stat player_Stat = new Player_Stat(0,0,0,0,0,0,0,0,0,0,0,0);
+            Database_Manipulator manipulator = new Database_Manipulator();
+            manipulator.Insert(player_Stat);
+        }
+
+        [TestMethod]
+        public void Insert_Team_Stat_Test()
+        {
+            Team_Stat team_Stat = new Team_Stat(0,0,0,0,0,0,0,0,0,0,0);
+            Database_Manipulator manipulator = new Database_Manipulator();
+            manipulator.Insert(team_Stat);
+        }
+
+        [TestMethod]
+        public void Insert_Team_Play_Test()
+        {
+            Player_Play team_play = new Player_Play(1,1);
+            Database_Manipulator manipulator = new Database_Manipulator();
+            manipulator.Insert(team_play);
+        }
+
+        [TestMethod]
+        public void Insert_Player_Play_Test()
+        {
+            Player_Play player_play = new Player_Play(1,1);
+            Database_Manipulator manipulator = new Database_Manipulator();
+            manipulator.Insert(player_play);
+        }
+
+
     }
 }
