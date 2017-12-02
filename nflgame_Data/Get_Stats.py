a13c1fa857f6e46f023f6eb3ca9c3d72e4b8f206
Nathan Stackpole <nstackpole21@gmail.com>
2017-12-02 13:00:32 -0500

    Get_Stats script now fetches 2016 season stats and prints them to data.txt

--- nflgame_Data/Get_Stats.py
+++ nflgame_Data/Get_Stats.py
@@ -3,13 +3,20 @@ import nflgame
 #Prints player name and number
 data_file = open('data.txt', 'w')
 
-players = nflgame.players
 games = nflgame.games(2016)
+game_stats = nflgame.combine_play_stats(games)
 
-for game in games:
-    data_file.write(game)
+# for game in games:
+#     data_file.write(game.home)
+#     data_file.write("\n")
 
+for player in game_stats:
+    data_file.write(player.name)
+    data_file.write(" ")
+    data_file.write(str(player.stats))
+    data_file.write("\n")
 
+data_file.close()
 # game = nflgame.games(2016, week=[1])
 # players = nflgame.combine(game)
 
