361b715c97ed1dcd316a029927cba7bc15b0aed1
Nathan Stackpole <nstackpole21@gmail.com>
2017-12-02 14:20:21 -0500

    Get_Stats.py now pull 40k play stats and puts them in data.txt

--- nflgame_Data/Get_Stats.py
+++ nflgame_Data/Get_Stats.py
@@ -4,17 +4,26 @@ import nflgame
 data_file = open('data.txt', 'w')
 
 games = nflgame.games(2016)
-game_stats = nflgame.combine_play_stats(games)
 
-# for game in games:
-#     data_file.write(game.home)
-#     data_file.write("\n")
+count = 0
 
-for player in game_stats:
-    data_file.write(player.name)
-    data_file.write(" ")
-    data_file.write(str(player.stats))
-    data_file.write("\n")
+for game in games:
+    game_stats = nflgame.combine_game_stats(games)
+
+    if count == 40000:
+        break
+
+
+    for player in game_stats:
+        
+        if count == 40000:
+            break
+
+        data_file.write(player.name)
+        data_file.write(" ")
+        data_file.write(str(player.stats))
+        data_file.write("\n")
+        count = count + 1
 
 data_file.close()
 # game = nflgame.games(2016, week=[1])
