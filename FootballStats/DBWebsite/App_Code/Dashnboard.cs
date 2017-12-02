c0864a38a05a149d21c4da97091e0af8c1843265
Matt <mam361@zips.uakron.edu>
2017-12-02 15:41:31 -0500

    Added outline for Dashboard controls

---
+++ FootballStats/DBWebsite/App_Code/Dashnboard.cs
@@ -0,0 +1,24 @@
+﻿using DatabaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Dashnboard
+/// </summary>
+public class Dashnboard
+{
+    public Dashnboard() { }
+
+
+    public static List<Player> getFavoritePlayers(string username)
+    {
+        return new List<Player>();
+    }
+
+    public static List<Team> getFavoriteTeams(string username)
+    {
+        return new List<Team>();
+    }
+}
