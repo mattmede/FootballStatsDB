826d6deb150676e696e4ef41594279225ca400fd
Matt <mam361@zips.uakron.edu>
2017-12-03 12:35:41 -0500

    More Dashboard Stuff

--- FootballStats/DBWebsite/App_Code/Dashboard.cs
+++ FootballStats/DBWebsite/App_Code/Dashboard.cs
@@ -16,7 +16,7 @@ public class Dashboard
 
     public List<Player> getFavoritePlayers(string username)
     {
-        string user_string = "SELECT User_Id FROM Users WHERE Username = \'" + username + "\'";
+        string user_string = "SELECT * FROM Users WHERE Username = \'" + username + "\'";
 
         SqlCommand command = new SqlCommand(user_string, connection);
 
@@ -35,13 +35,12 @@ public class Dashboard
 
         connection.Close();
 
-        string favPlayers_string = "SELECT * FROM Fav_Players WHERE User_Id = " + user.Id;
+        string favPlayers_string = "SELECT * FROM Fav_Player WHERE User_Id = " + user.Id;
 
         command = new SqlCommand(favPlayers_string, connection);
 
         connection.Open();
         reader = command.ExecuteReader();
-        connection.Close();
 
         //run when you get fav players
         if (reader.HasRows)
@@ -53,11 +52,51 @@ public class Dashboard
             }
         }
 
+        connection.Close();
+
         return players;
     }
 
     public List<Team> getFavoriteTeams(string username)
     {
-        return new List<Team>();
+        string user_string = "SELECT * FROM Users WHERE Username = \'" + username + "\'";
+
+        SqlCommand command = new SqlCommand(user_string, connection);
+
+        connection.Open();
+        var reader = command.ExecuteReader();
+        var teams = new List<Team>();
+        var user = new User();
+
+        if (reader.HasRows)
+        {
+            while (reader.Read())
+            {
+                user = new User(reader.GetString(1), reader.GetString(2), reader.GetInt32(0));
+            }
+        }
+
+        connection.Close();
+
+        string favTeams_string = "SELECT * FROM Fav_Team WHERE User_Id = " + user.Id;
+
+        command = new SqlCommand(favTeams_string, connection);
+
+        connection.Open();
+        reader = command.ExecuteReader();
+
+        //run when you get fav players
+        if (reader.HasRows)
+        {
+            while (reader.Read())
+            {
+                var player = new Team(reader.GetString(1), reader.GetInt32(0));
+                teams.Add(player);
+            }
+        }
+
+        connection.Close();
+
+        return teams;
     }
 }
