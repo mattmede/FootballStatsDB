fb8e1cefb3f7c3a42c3f72520e71bafbe8bbdbb7
Nathan Stackpole <nstackpole21@gmail.com>
2017-12-03 11:48:45 -0500

    Get_Stats now goes week by week getting stats for players and teams and putting them in player_stats.txt and team_stats.txt respectively

--- nflgame_Data/Get_Stats.py
+++ nflgame_Data/Get_Stats.py
@@ -1,23 +1,42 @@
 import nflgame
 
 #Prints player name and number
-data_file = open('data.txt', 'w')
+player_stats_file = open('player_stats.txt', 'w')
+team_stats_file = open('team_stats.txt', 'w')
 
-games = nflgame.games(2016)
+
 
 #Gets the max_stats for each play in each game of the 2016 season and writes them to data.txt
-for game in games:
-    game_stats = game.max_player_stats()
+for week in range(1,18):
 
-    for stat in game_stats:
-        data_file.write(str(stat.player))
-        data_file.write(" Team: ")
-        data_file.write(str(stat.team))
-        data_file.write(" ")
-        data_file.write(str(stat.formatted_stats()))
-        data_file.write("\n")
+    games = nflgame.games(2016, week)
+    
+    for game in games:
+        game_stats = game.max_player_stats()
 
-data_file.close()
+        for stat in game_stats:
+            player_stats_file.write(str(stat.player))
+            player_stats_file.write(" Team: ")
+            player_stats_file.write(str(stat.team))
+            player_stats_file.write(" Week: ")
+            player_stats_file.write(str(week))
+            player_stats_file.write(" ")
+            player_stats_file.write(str(stat.formatted_stats()))
+            player_stats_file.write("\n")
+        
+        team_stats_file.write(str(game.home))
+        team_stats_file.write(" Week: ")
+        team_stats_file.write(str(week))
+        team_stats_file.write(" ")
+        team_stats_file.write(str(game.stats_home))
+        team_stats_file.write("\n")
+        team_stats_file.write(str(game.away))
+        team_stats_file.write(" Week: ")
+        team_stats_file.write(str(week))
+        team_stats_file.write(" ")
+        team_stats_file.write(str(game.stats_away))
+
+player_stats_file.close()
 # game = nflgame.games(2016, week=[1])
 # players = nflgame.combine(game)
 
