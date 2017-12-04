5ac1f0eaa1cb6ef3352d07b23740cf62dddd65bb
Matt <mam361@zips.uakron.edu>
2017-12-03 22:54:02 -0500

    STATSgit add *

--- FootballStats/DBWebsite/App_Code/LoadPlayers.cs
+++ FootballStats/DBWebsite/App_Code/LoadPlayers.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using DatabaseManipulation;
 using DatabaseModels;
+using System.Data.SqlClient;
 
 /// <summary>
 /// Summary description for LoadPlayers
@@ -12,13 +13,60 @@ public class LoadPlayers
 {
     public LoadPlayers() { }
 
-    public  static List<Player> GetPlayers()
+    private static SqlConnection connection = new SqlConnection(@"Data Source=(local);Initial Catalog=FootBallStatsDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+    public  static List<StatPlayer> GetPlayers()
     {
         var dbManipulator = new Database_Manipulator();
         var players = new List<Player>();
+        var statPlayers = new List<StatPlayer>();
+
+        SqlCommand command;
 
         players = dbManipulator.SelectAllEntries();
 
-        return players;
+        foreach(Player p in players)
+        {
+            var statIds = new List<int>();
+            var stats = new List<Player_Stat>();
+            string stat_string = "SELECT Player_Stat_Id FROM Player_Plays WHERE Player_Id = " + p.Id;
+            command = new SqlCommand(stat_string, connection);
+
+            connection.Open();
+            var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    statIds.Add(reader.GetInt32(0));
+                }
+            }
+            connection.Close();
+
+            foreach (int stat in statIds)
+            {
+                string getStats = "SELECT * FROM Player_Stats WHERE Player_Stat_Id = " + stat;
+
+                command = new SqlCommand(getStats, connection);
+                connection.Open();
+                reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        stats.Add(new Player_Stat(reader.GetInt32(1), reader.GetInt32(2), reader.GetInt32(3),
+                            reader.GetInt32(4), reader.GetInt32(5), reader.GetInt32(6), reader.GetInt32(7),
+                            reader.GetInt32(8), reader.GetInt32(9), reader.GetInt32(10), reader.GetInt32(11),
+                            reader.GetInt32(12), reader.GetInt32(0)));
+                    }
+                }
+                connection.Close();
+            }
+            statPlayers.Add(new StatPlayer(p, stats));
+        }
+
+        return statPlayers;
     }
 }
---
+++ FootballStats/DBWebsite/App_Code/StatPlayer.cs
@@ -0,0 +1,68 @@
+﻿using DatabaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class StatPlayer
+{
+    public Player player;
+    public List<Player_Stat> player_stats;
+
+    public StatPlayer()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public StatPlayer(Player p, List<Player_Stat> st)
+    {
+        player = p;
+        player_stats = st;
+    }
+}
+
+public class SummedPlayerStats
+{
+    public int total_yards;
+    public int rush_yards;
+    public int pass_yards;
+    public int receiving_yards;
+    public int tds;
+    public int fumbles;
+    public int interceptions_thrown;
+    public int tackles;
+    public int sacks;
+    public int ffs;
+    public int interceptions;
+
+    public SummedPlayerStats()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public SummedPlayerStats(List<Player_Stat> st)
+    {
+        foreach(Player_Stat ps in st)
+        {
+            rush_yards += ps.Rush_Yards;
+            pass_yards += ps.Pass_Yards;
+            receiving_yards += ps.Receiving_Yards;
+            tds += ps.TDs;
+            fumbles += ps.Fumbles;
+            interceptions_thrown += ps.Interceptions_Thrown;
+            tackles += ps.Tackles;
+            sacks += ps.Sacks;
+            ffs += ps.Forced_Fumbles;
+            interceptions += ps.Interceptions;
+        }
+
+        total_yards = rush_yards + pass_yards + receiving_yards;
+    }
+}
