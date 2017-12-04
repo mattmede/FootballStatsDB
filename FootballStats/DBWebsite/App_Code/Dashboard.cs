526b44a816db49c1368afda53ee0592ec0c71bda
Matt <mam361@zips.uakron.edu>
2017-12-03 23:12:08 -0500

    stats in dashboard

--- FootballStats/DBWebsite/App_Code/Dashboard.cs
+++ FootballStats/DBWebsite/App_Code/Dashboard.cs
@@ -14,7 +14,7 @@ public class Dashboard
 
     private SqlConnection connection = new SqlConnection(@"Data Source=(local);Initial Catalog=FootBallStatsDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 
-    public List<Player> getFavoritePlayers(string username)
+    public List<StatPlayer> getFavoritePlayers(string username)
     {
         string user_string = "SELECT * FROM Users WHERE Username = \'" + username + "\'";
 
@@ -24,6 +24,7 @@ public class Dashboard
         var reader = command.ExecuteReader();
         var playerIds = new List<int>();
         var players = new List<Player>();
+        var statPlayers = new List<StatPlayer>();
         var user = new User();
 
         if (reader.HasRows)
@@ -70,7 +71,49 @@ public class Dashboard
         }
         connection.Close();
 
-        return players;
+        foreach (Player p in players)
+        {
+            var statIds = new List<int>();
+            var stats = new List<Player_Stat>();
+            string stat_string = "SELECT Player_Stat_Id FROM Player_Plays WHERE Player_Id = " + p.Id;
+            command = new SqlCommand(stat_string, connection);
+
+            connection.Open();
+            reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    statIds.Add(reader.GetInt32(0));
+                }
+            }
+            connection.Close();
+
+            foreach (int stat in statIds)
+            {
+                string getStats = "SELECT * FROM Player_Stats WHERE Player_Stat_Id = " + stat;
+
+                command = new SqlCommand(getStats, connection);
+                connection.Open();
+                reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        stats.Add(new Player_Stat(reader.GetInt32(1), reader.GetInt32(2), reader.GetInt32(3),
+                            reader.GetInt32(4), reader.GetInt32(5), reader.GetInt32(6), reader.GetInt32(7),
+                            reader.GetInt32(8), reader.GetInt32(9), reader.GetInt32(10), reader.GetInt32(11),
+                            reader.GetInt32(12), reader.GetInt32(0)));
+                    }
+                }
+                connection.Close();
+            }
+            statPlayers.Add(new StatPlayer(p, stats));
+        }
+
+        return statPlayers;
     }
 
     public List<Team> getFavoriteTeams(string username)
