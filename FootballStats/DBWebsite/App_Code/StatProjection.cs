6d6fdffcb88ed9d40b2f6e5ca8e5d7fc650412e3
Nathan Stackpole <nstackpole21@gmail.com>
2017-12-04 09:20:14 -0500

    Added team stat projection

--- FootballStats/DBWebsite/App_Code/StatProjection.cs
+++ FootballStats/DBWebsite/App_Code/StatProjection.cs
@@ -20,14 +20,14 @@ public class StatProjection
         //
     }
 
-    public Player_Stat GetProjectedStats(string player_name)
+    public Player_Stat GetProjectedPlayerStats(string player_name)
     {
 
         int player_id = GetPlayerId(player_name);
 
         List<int> stat_ids = GetPlayerStatsIds(player_id);
 
-        return ProjectStats(stat_ids);
+        return ProjectPlayerStats(stat_ids);
       
     }
 
@@ -91,7 +91,7 @@ public class StatProjection
     }
 
 
-    private Player_Stat ProjectStats(List<int> stat_ids)
+    private Player_Stat ProjectPlayerStats(List<int> stat_ids)
     {
         int rush_yards = 0, pass_yards = 0, receiving_yards = 0, tds = 0, fumbles = 0, interceptions_thrown = 0, tackles = 0, forced_fumbles = 0, interceptions = 0;
         int sacks = 0;
@@ -127,5 +127,109 @@ public class StatProjection
 
         return new Player_Stat(0, 2016, rush_yards, pass_yards, receiving_yards, tds, fumbles, interceptions_thrown, tackles, sacks, forced_fumbles, interceptions);
     }
-    
+
+
+    public Team_Stat GetProjectedTeamStats(string team_name)
+    {
+        int team_id = GetTeamId(team_name);
+
+        List<int> stat_ids = GetTeamStatsIds(team_id);
+
+        return ProjectTeamStats(stat_ids);
+    }
+
+    private int GetTeamId(string name)
+    {
+        string find_player_id_command = @"SELECT Team_Id FROM Teams WHERE Team_Name = '" + name + @"';";
+
+        SqlCommand command = new SqlCommand(find_player_id_command, connection);
+
+        connection.Open();
+        var reader = command.ExecuteReader();
+
+        reader.Read();
+
+        int team_id = reader.GetInt32(0);
+
+        connection.Close();
+
+        return team_id;
+    }
+
+    private List<int> GetTeamStatsIds(int team_id)
+    {
+        string get_plays_command = "SELECT Team_Stat_Id FROM Team_Plays WHERE Team_Id = " + team_id + ";";
+
+        List<int> ids = new List<int>();
+
+        SqlCommand command = new SqlCommand(get_plays_command, connection);
+
+        connection.Open();
+
+        var reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            ids.Add(reader.GetInt32(0));
+        }
+
+        connection.Close();
+
+        return ids;
+    }
+
+    private Team_Stat GetTeamStat(int id)
+    {
+        string get_stats_command = "SELECT (Rush_Yards, Pass_Yards, First_Downs, Total_Yards, Penalty_Count, Penalty_Yards, Turnovers, Punt_Count, Punt_Yards, Punt_Average) FROM Team_Stats WHERE Team_Stat_Id = " + id;
+        SqlCommand command = new SqlCommand(get_stats_command, connection);
+
+        connection.Open();
+
+        var reader = command.ExecuteReader();
+
+        reader.Read();
+
+        Team_Stat stat = new Team_Stat(0, 2016, reader.GetInt32(0), reader.GetInt32(1), reader.GetInt32(2), reader.GetInt32(3), reader.GetInt32(4), reader.GetInt32(5), reader.GetInt32(6), reader.GetInt32(7), reader.GetInt32(8), reader.GetInt32(9));
+
+        connection.Close();
+
+        return stat;
+
+    }
+
+
+    private Team_Stat ProjectTeamStats(List<int> stat_ids)
+    {
+        int rush_yards = 0, pass_yards = 0, first_downs = 0, total_yards = 0, penalty_count = 0, penalty_yards = 0, turnovers = 0, punt_count = 0, punt_yards = 0, punt_average = 0;
+
+        foreach (int id in stat_ids)
+        {
+            Team_Stat stat = GetTeamStat(id);
+
+            rush_yards += stat.Rush_Yards;
+            pass_yards += stat.Pass_Yards;
+            first_downs += stat.First_Downs;
+            total_yards += stat.Total_Yards;
+            penalty_count += stat.Penalty_Count;
+            penalty_yards += stat.Penalty_Yards;
+            turnovers += stat.Turnovers;
+            punt_count += stat.Punt_Count;
+            punt_yards += stat.Punt_Yards;
+            punt_average += stat.Punt_Average;
+
+        }
+
+        rush_yards /= stat_ids.Count;
+        pass_yards /= stat_ids.Count;
+        first_downs /= stat_ids.Count;
+        total_yards /= stat_ids.Count;
+        penalty_count /= stat_ids.Count;
+        penalty_yards /= stat_ids.Count;
+        turnovers /= stat_ids.Count;
+        punt_count /= stat_ids.Count;
+        punt_yards /= stat_ids.Count;
+        punt_average /= stat_ids.Count;
+
+        return new Team_Stat(0, 2016, rush_yards, pass_yards, first_downs, total_yards, penalty_count, penalty_yards, turnovers, punt_count, punt_yards, punt_average);
+    }
 }
